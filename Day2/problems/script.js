c295fc0ab358bd208b3af133a6b4fcf32f0be330
Ahmad Raza <razaprivateltd31@gmail.com>
2025-11-23 15:13:31 +0530

    problem 3 done sum even using filter reduce

--- Day2/problems/script.js
+++ Day2/problems/script.js
@@ -3,13 +3,21 @@
 // using map filter reduce
 
 //Remove duplicates from array WITHOUT using loops:
-function removeduplicates(arr){
-    //arr.filter(a => indexOf(a)=== )
+// function removeduplicates(arr){
+//     //arr.filter(a => indexOf(a)=== )
+// }
+
+// problem 2 to uppercase arr 
+// function toUpper(arr){
+//     arr = arr.map((a)=>a.toUpperCase());
+//     return arr;
+// }
+// console.log(toUpper(["ahmad","raza"]));
+
+// --->> problem 3 find sum of all even numbers in array
+
+function sumeven(arr){
+    return arr.filter(a=> a%2===0).reduce((a,b)=> a+b,0)
 }
 
-// problem to uppercase arr 
-function toUpper(arr){
-    arr = arr.map((a)=>a.toUpperCase());
-    return arr;
-}
-console.log(toUpper(["ahmad","raza"]));
+console.log(sumeven([10, 21, 33, 40, 55, 62]));
