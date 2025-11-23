b132f0062db3e370747e45a9cc0889aff5268629
Ahmad Raza <razaprivateltd31@gmail.com>
2025-11-23 15:01:23 +0530

    dual commit remove duplicates

---
+++ Day2/problems/script.js
@@ -0,0 +1,8 @@
+// day 2 section 2
+// Problem 1 (Easy)
+// using map filter reduce
+
+//Remove duplicates from array WITHOUT using loops:
+function removeduplicates(arr){
+    arr.filter(a => indexOf(a)===)
+}
